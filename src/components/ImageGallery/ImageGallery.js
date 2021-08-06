e61c0bf4a7252c28db1f1a4efd61eefad6a6bf23
Alona-Suprun <a.alonka.l@gmail.com>
2021-08-06 10:28:58 +0300

    add loadMore button

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 
+import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Button from '../Button/Button';
 
 import ImageApi from '../../api/imageApi';
 
@@ -16,6 +18,7 @@ class ImageGallery extends Component {
     image: null,
     error: null,
     status: 'idle',
+    loading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -23,40 +26,71 @@ class ImageGallery extends Component {
     const nextSearch = this.props.searchValue;
 
     if (prevSearch !== nextSearch) {
-      this.setState({ status: 'pending' });
+      this.setState({ status: 'pending', loading: true });
 
       imageApi.query = this.props.searchValue;
       imageApi
         .fetchImagesApi(nextSearch)
         .then(image => {
           if (image.length === 0) {
-            toast(
-              `Sorry, there is no image of ${this.props.searchValue}! Try again.`,
-            );
-          }
-          this.setState({ image, status: 'resolved' });
+            toast(`Sorry, there is no image of ${this.props.searchValue}!`);
+
+            this.setState({ image, status: 'rejected' });
+            return;
+          } else this.setState({ image, status: 'resolved' });
         })
-        .catch(error => this.setState({ error, status: 'rejected' }));
+        .catch(error => {
+          this.setState({ error, status: 'rejected' });
+        });
     }
   }
 
+  onClick = () => {
+    const scrollTo = document.documentElement.scrollHeight - 141;
+    imageApi
+      .fetchImagesApi()
+      .then(image => {
+        this.setState(prevState => ({
+          loading: !prevState.loading,
+          image: [...prevState.image, ...image],
+        }));
+        window.scrollTo({
+          top: scrollTo,
+          behavior: 'smooth',
+        });
+      })
+      .finally(
+        this.setState(prevState => ({
+          loading: !prevState.loading,
+        })),
+      );
+  };
+
   render() {
-    const { image, status } = this.state;
+    const { image, status, loading } = this.state;
 
     if (status === 'idle') {
       return (
-        <h1 className={s.heading}>
+        <h2 className={s.heading}>
           Type what kind of image you're looking for in the search bar.
-        </h1>
+        </h2>
       );
     }
 
     if (status === 'pending') {
-      return <Loader />;
+      return (
+        <div
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          <Loader type="Bars" />
+        </div>
+      );
     }
 
     if (status === 'rejected') {
-      return <p>Try again.</p>;
+      return <h2 className={s.heading}>Try again.</h2>;
     }
 
     if (status === 'resolved') {
@@ -65,10 +99,36 @@ class ImageGallery extends Component {
           <ul className={s.imageGallery}>
             <ImageGalleryItem image={image} />
           </ul>
+          {loading ? (
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: '20px',
+                marginBottom: '20px',
+              }}
+            >
+              <Button type="button" onClick={this.onClick}>
+                Load more
+              </Button>
+            </div>
+          ) : (
+            <div
+              style={{
+                textAlign: 'center',
+              }}
+            >
+              <Loader type="Bars" />
+            </div>
+          )}
         </>
       );
     }
   }
 }
+
+ImageGallery.propTypes = {
+  onClick: PropTypes.func,
+  searchValue: PropTypes.string.isRequired,
+};
 
 export default ImageGallery;
